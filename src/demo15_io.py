94c99b94fa11831b42ea73cee90b61025b9ceac3
theape <theape@163.com>
2020-06-25 17:53:20 +0800

    feat: 文件操作 序列化

--- src/demo15_io.py
+++ src/demo15_io.py
@@ -2,6 +2,7 @@
 # _*_ coding:utf-8 _*_
 
 import math as m
+import pickle
 
 # 输入和输出 IO
 
@@ -26,8 +27,8 @@ print("浮点数{0:.3f}".format(m.pi))
 
 # 引用全局方法 !a ascii() !s str() !r repr()
 print("引用ASCII，{!a}".format("a"), "ascii()", ascii('\n'))
-print("引用STRING，{!s}".format("str\t\b\s\r\n"))
-print("引用REPRESENT，{!r}".format("repr\t\b\s\r\n"))
+print("引用STRING，{!s}".format("str\t\b\r\n"))
+print("引用REPRESENT，{!r}".format("repr\t\b\r\n"))
 
 # 0填充，不足位数补0，包括小数；超过位数不处理
 print('12'.zfill(5))
@@ -40,19 +41,94 @@ print("我是右对齐".rjust(20))
 print("我是中心对齐".center(20))
 
 # 传入字典格式化
-table = { 'key1': 1, 'key2': 2 }
+table = {'key1': 1, 'key2': 2}
 print("传入字典格式化，{0[key1]} {0[key2]}".format(table))
 print("传入字典格式化，{key1} {key2}".format(**table))
 print("传入字典格式化，{key1:d} {key2:d}".format(**table))
 print("传入字典格式化，{key1:2d} {key2:2d}".format(**table))
 
 # 传统的字符串格式化
-print("传统的字符串格式化，%s"%'「我是字符串」')
+print("传统的字符串格式化，%s" % '「我是字符串」')
+print("传统的字符串格式化，%i" % 1)
+print("传统的字符串格式化，%f" % 1.2)
+print("传统的字符串格式化，%d" % 12)
+print("传统的字符串格式化，多个参数：%d %i %f %s" % (12, 1, 1.1, 'str'))
+
+# 新式格式化
+format_param1 = "参数1"
+format_param2 = "参数2"
+print(f'新式格式化 {format_param1} {format_param2}')
+print(f'新式格式化 {12} {32}')
+print(f'新式格式化 {"参数1"} {"参数2"}')
 
 # 二、读取键盘输入
-
+# inp = input("请输入：")
+# print("你输入了：", inp)
 
 # 三、读写文件
 
 
+def read_file(file_path):
+    """
+    读文件
+    :param file_path: 文件路径
+    :return: 无
+    """
+    fr = open(file_path, 'r')
+    print(fr.readlines())
+    fr.close()
+
+
+def write_file(file_path, context):
+    """
+    以覆盖方式写文件
+    :param file_path: 文件路径
+    :param context: 内容
+    :return: 无
+    """
+    fw = open('files/write.txt', 'w')
+    fw.write(context)
+    fw.close()
+
+
+def append_file(file_path, context):
+    """
+    以追加方式写文件
+    :param file_path: 文件路径
+    :param context: 内容
+    :return: 无
+    """
+    a = open(file_path, 'a')
+    a.writelines(context)
+    a.close()
+
+
+# 读文件
+f = open('files/read.txt', 'r')
+print(f.read())
+f.close()
+
+# 写文件
+write_file('files/write.txt', "# This file is for writing.\n")
+read_file('files/write.txt')
+# 以追加方式写文件
+append_file('files/write.txt', "# This file is for appending.")
+read_file('files/write.txt')
+
+# 清除内容
+write_file('files/write.txt', "")
+read_file('files/write.txt')
+
 # 四、序列化模块 pickle
+obj = {'key1': 1, 'key2': 2}
+wb = open("files/obj.ser", "wb")
+# 序列化
+pickle.dump(obj, wb)
+wb.close()
+
+# 反序列化
+wb = open("files/obj.ser", "rb")
+load_obj = pickle.load(wb)
+wb.close()
+print(load_obj)
+
